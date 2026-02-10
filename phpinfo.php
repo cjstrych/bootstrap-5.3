0a3bcdbfe6a5bbe66b2a51969b8171b6a2a557d1
cjstrych <184395759+cjstrych@users.noreply.github.com>
2026-02-10 14:57:38 -0500

    2026-02-10 - phpinfo.php

--- phpinfo.php
+++ phpinfo.php
@@ -1,10 +1,29 @@
 <?php
 
-// SHOW ALL INFORMATION, DEFAULTS TO INFO_ALL
+/**
+ * PHP INFORMATION REFERENCE
+ * USE THESE CONSTANTS AS ARGUMENTS IN PHPINFO() TO TOGGLE SPECIFIC DATA.
+ */
+
+/*
+| CONSTANT           | VALUE | DESCRIPTION                                     |
+|--------------------|-------|-------------------------------------------------|
+| INFO_GENERAL       | 1     | SYSTEM INFO, BUILD DATE, SERVER, ETC.           |
+| INFO_CREDITS       | 2     | PHP CREDITS (THE "WHO'S WHO" OF PHP)            |
+| INFO_CONFIGURATION | 4     | LOCAL AND MASTER PHP.INI DIRECTIVES             |
+| INFO_MODULES       | 8     | LOADED EXTENSIONS AND THEIR SPECIFIC SETTINGS   |
+| INFO_ENVIRONMENT   | 16    | $_ENV VARIABLE INFORMATION                      |
+| INFO_VARIABLES     | 32    | ALL PREDEFINED VARIABLES (EGPCS)                |
+| INFO_LICENSE       | 64    | PHP LICENSE INFORMATION                         |
+| INFO_ALL           | -1    | SHOWS EVERYTHING (DEFAULT)                      |
+*/
+
+// --- ACTIVE COMMANDS ---
+
+// SHOW EVERYTHING
 phpinfo();
 
-// SHOW JUST THE MODULE INFORMATION.
-// PHPINFO(8) YIELDS IDENTICAL RESULTS.
-phpinfo(INFO_MODULES);
+// SHOW ONLY MODULE/EXTENSION INFORMATION
+phpinfo(INFO_MODULES); 
 
 ?>
